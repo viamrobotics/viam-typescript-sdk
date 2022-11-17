be0dafcce6de0f47653d249b1d16913b38e076f3
Maxim Pertsov <maxim@viam.com>
2022-11-17 11:31:30 -0500

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,28 @@
-import { defineConfig } from 'vite'
-import path from 'node:path'
-import dts from 'vite-plugin-dts'
+import { defineConfig } from "vite";
+import path from "node:path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    'process.env.NODE_ENV': '"production"'
+    "process.env.NODE_ENV": '"production"',
   },
   plugins: [dts()],
   build: {
+    minify: true,
     target: 'esnext',
-    assetsInlineLimit: 0,
     lib: {
-      entry: path.resolve(__dirname, 'src/main.ts'),
-      name: 'vite-typescript-sdk',
-      fileName: (format) => `main.${format}.js`
+      entry: path.resolve(__dirname, "src/main.ts"),
+      name: "vite-typescript-sdk",
+      fileName: (format) => `main.${format}.js`,
     },
     rollupOptions: {
-      output: {
-        inlineDynamicImports: true,
-        manualChunks: undefined,
+      onwarn: (warning, warn) => {
+        if (warning.code === "EVAL") {
+          return;
+        }
+        warn(warning);
       },
     },
-  }
-})
+  },
+});
