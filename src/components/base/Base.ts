85aab592398e784be476725e5899dfac37336803
Maxim Pertsov <maxim@viam.com>
2022-12-19 15:05:15 -0500

    centralize extra params type

--- src/components/base/Base.ts
+++ src/components/base/Base.ts
@@ -1,11 +1,10 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 import type { baseApi, commonApi } from '../../main'
+import type { Extra } from '../../types'
 
 export interface Base {
-  moveStraight(distanceMm: number, mmPerSec: number, extra?: Map<string, any>): Promise<baseApi.MoveStraightResponse>
-  spin(angleDeg: number, degsPerSec: number, extra?: Map<string, any>): Promise<baseApi.SpinResponse>
-  setPower(linear: commonApi.Vector3, angular: commonApi.Vector3, extra?: Map<string, any>): Promise<baseApi.SetPowerResponse>
-  setVelocity(linear: commonApi.Vector3, angular: commonApi.Vector3, extra?: Map<string, any>): Promise<baseApi.SetVelocityResponse>
-  stop(extra?: Map<string, any>): Promise<baseApi.StopResponse>
+  moveStraight(distanceMm: number, mmPerSec: number, extra?: Extra): Promise<baseApi.MoveStraightResponse>
+  spin(angleDeg: number, degsPerSec: number, extra?: Extra): Promise<baseApi.SpinResponse>
+  setPower(linear: commonApi.Vector3, angular: commonApi.Vector3, extra?: Extra): Promise<baseApi.SetPowerResponse>
+  setVelocity(linear: commonApi.Vector3, angular: commonApi.Vector3, extra?: Extra): Promise<baseApi.SetVelocityResponse>
+  stop(extra?: Extra): Promise<baseApi.StopResponse>
 }
--- src/components/motor/Motor.ts
+++ src/components/motor/Motor.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { Extra } from '../../types'
 import type { motorApi } from '../../main'
 
 export interface Properties {
@@ -6,12 +6,12 @@ export interface Properties {
 }
 
 export interface Motor {
-  setPower(power: number, extra?: Map<string, any>): Promise<motorApi.SetPowerResponse>;
-  goFor(rpm: number, revolutions: number, extra?: Map<string, any>): Promise<motorApi.GoForResponse>;
-  goTo(rpm: number, positionRevolutions: number, extra?: Map<string, any>): Promise<motorApi.GoToResponse>;
-  resetZeroPosition(offset: number, extra?: Map<string, any>): Promise<motorApi.ResetZeroPositionResponse>;
-  motorStop(extra?: Map<string, any>): Promise<motorApi.StopResponse>;
-  getProperties(extra?: Map<string, any>): Promise<Properties>;
-  getPosition(extra?: Map<string, any>): Promise<number>;
-  isPowered(extra?: Map<string, any>): Promise<readonly [boolean, number]>;
+  setPower(power: number, extra?: Extra): Promise<motorApi.SetPowerResponse>;
+  goFor(rpm: number, revolutions: number, extra?: Extra): Promise<motorApi.GoForResponse>;
+  goTo(rpm: number, positionRevolutions: number, extra?: Extra): Promise<motorApi.GoToResponse>;
+  resetZeroPosition(offset: number, extra?: Extra): Promise<motorApi.ResetZeroPositionResponse>;
+  motorStop(extra?: Extra): Promise<motorApi.StopResponse>;
+  getProperties(extra?: Extra): Promise<Properties>;
+  getPosition(extra?: Extra): Promise<number>;
+  isPowered(extra?: Extra): Promise<readonly [boolean, number]>;
 }
