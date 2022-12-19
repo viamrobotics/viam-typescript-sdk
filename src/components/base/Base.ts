e001e007d0ef0a108e442417c056e0e8825de416
Maxim Pertsov <maxim@viam.com>
2022-12-19 13:25:46 -0500

    space after colon

--- src/components/base/Base.ts
+++ src/components/base/Base.ts
@@ -3,9 +3,9 @@
 import type { baseApi, commonApi } from '../../main'
 
 export interface Base {
-  moveStraight(distanceMm: number, mmPerSec: number, extra?:Map<string, any>):Promise<baseApi.MoveStraightResponse>
-  spin(angleDeg:number, degsPerSec:number, extra?:Map<string, any>):Promise<baseApi.SpinResponse>
-  setPower(linear:commonApi.Vector3, angular:commonApi.Vector3, extra?:Map<string, any>):Promise<baseApi.SetPowerResponse>
-  setVelocity(linear:commonApi.Vector3, angular:commonApi.Vector3, extra?:Map<string, any>):Promise<baseApi.SetVelocityResponse>
-  stop(extra?:Map<string, any>):Promise<baseApi.StopResponse>
+  moveStraight(distanceMm: number, mmPerSec: number, extra?: Map<string, any>): Promise<baseApi.MoveStraightResponse>
+  spin(angleDeg: number, degsPerSec: number, extra?: Map<string, any>): Promise<baseApi.SpinResponse>
+  setPower(linear: commonApi.Vector3, angular: commonApi.Vector3, extra?: Map<string, any>): Promise<baseApi.SetPowerResponse>
+  setVelocity(linear: commonApi.Vector3, angular: commonApi.Vector3, extra?: Map<string, any>): Promise<baseApi.SetVelocityResponse>
+  stop(extra?: Map<string, any>): Promise<baseApi.StopResponse>
 }
