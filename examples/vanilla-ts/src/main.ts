d19ff47979adbef62196cd46090e0aeb0f1b86c6
Maxim Pertsov <maxim@viam.com>
2022-12-30 15:39:23 -0500

    connect to local robot with sdk

--- examples/vanilla-ts/src/main.ts
+++ examples/vanilla-ts/src/main.ts
@@ -1,5 +1,24 @@
-const yodawg = () => {
-  console.log("yo dawg");
-};
+import { Client } from "@viamrobotics/sdk";
+import type { Credentials } from "@viamrobotics/rpc/src/dial";
 
-yodawg();
+async function connect() {
+  // TODO: update CORS config to allow us to connect to real robot
+  const client = new Client("http://localhost:8080");
+  await client.connect();
+  return client;
+}
+
+async function main() {
+  let client: Client;
+  try {
+    let returnVal = await connect();
+    client = returnVal;
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+  console.log(client?.robotService.resourceNames);
+  return;
+}
+
+main();
