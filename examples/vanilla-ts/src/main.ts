dbcd4f56883922e2b0397cec5f99469a87ac2dfb
Maxim Pertsov <maxim@viam.com>
2023-01-05 18:37:20 -0500

    setup non-local connect example

--- examples/vanilla-ts/src/main.ts
+++ examples/vanilla-ts/src/main.ts
@@ -1,9 +1,21 @@
 import { Client, MotorClient } from "@viamrobotics/sdk";
 import type { Credentials } from "@viamrobotics/rpc/src/dial";
 
-async function connect() {
-  // TODO: update CORS config to allow us to connect to real robot
+async function connect_insecure() {
   const client = new Client("http://localhost:8080");
+  await client.connect();
+  return client;
+}
+
+async function connect() {
+  let creds: Credentials = {
+    payload: "<SECRET>",
+    type: "robot-location-secret",
+  };
+
+  const client = new Client("<HOST>");
+  await client.connect("authEntity", creds);
+
   await client.connect();
   return client;
 }
@@ -13,6 +25,7 @@ async function main() {
   try {
     let returnVal = await connect();
     client = returnVal;
+    console.log("connected!");
   } catch (error) {
     console.log(error);
     return;
@@ -21,7 +34,7 @@ async function main() {
   const mc = new MotorClient(client, "motor1");
   console.log(await mc.getPosition());
 
-  await mc.goFor(100, 10)
+  await mc.goFor(100, 10);
 
   console.log(await mc.getPosition());
 
