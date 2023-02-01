8622cf96225ea450bc59adbcdbb780b043f9390b
Maxim Pertsov <maxim@viam.com>
2023-02-01 15:37:40 -0500

    add a button the example app

---
+++ examples/README.md
@@ -0,0 +1,10 @@
+# Viam SDK Quickstart
+
+This example demonstrates how to connect to a robot and operate a motor on it.
+
+## Usage
+
+Run `npm run watch` and visit `localhost:8000` in a browser.
+
+Edit `src/main.ts` to change the robot logic being run.
+Edit `static/index.html` to change the layout of the app.
--- examples/vanilla/src/main.ts
+++ examples/vanilla/src/main.ts
@@ -1,43 +1,63 @@
 import { Client, MotorClient } from "@viamrobotics/sdk";
 import type { Credentials } from "@viamrobotics/rpc/src/dial";
 
-async function connect_insecure() {
-  const client = new Client("http://localhost:8080");
-  await client.connect();
-  return client;
-}
-
 async function connect() {
-  let creds: Credentials = {
-    payload: "<SECRET>",
+  // You can remove this block entirely if your robot is not authenticated.
+  // Otherwise, replace with an actual secret.
+  const secret = "<SECRET>"
+  const creds: Credentials = {
+    payload: secret,
     type: "robot-location-secret",
   };
 
-  const client = new Client("<HOST>");
+  // Replace with the host of your actual robot running Viam.
+  const host = "<HOST>"
+  const client = new Client(host);
+
+  // Omit `creds` if your robot is not authenticated.
   await client.connect(undefined, creds);
 
   return client;
 }
 
+function button() {
+  return <HTMLButtonElement>document.getElementById("main-button");
+}
+
+// This function runs a motor named "motor1" on your robot.
+// Feel free to replace it whatever logic you want to test out!
+async function run(client: Client) {
+  // Replace with the name of a motor on your robot.
+  const name = "<MOTOR NAME>";
+  const mc = new MotorClient(client, name);
+
+  try {
+    button().disabled = true;
+
+    console.log(await mc.getPosition());
+    await mc.goFor(100, 10);
+    console.log(await mc.getPosition());
+  } finally {
+    button().disabled = false;
+  }
+}
+
 async function main() {
+  // Connect to client
   let client: Client;
   try {
-    let returnVal = await connect();
-    client = returnVal;
+    client = await connect();
     console.log("connected!");
   } catch (error) {
     console.log(error);
     return;
   }
 
-  const mc = new MotorClient(client, "motor1");
-  console.log(await mc.getPosition());
-
-  await mc.goFor(100, 10);
-
-  console.log(await mc.getPosition());
-
-  return;
+  // Make the button in our app do something interesting
+  button().onclick = async () => {
+    await run(client);
+  };
+  button().disabled = false;
 }
 
 main();
