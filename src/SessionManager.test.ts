2661255904fe56daa52f4b13e553b118f6c56f2a
Maxim Pertsov <maxim@viam.com>
2023-02-07 14:44:20 -0500

    more tests

--- src/SessionManager.test.ts
+++ src/SessionManager.test.ts
@@ -1,15 +1,10 @@
 // @vitest-environment happy-dom
 
-import {
-  afterEach,
-  beforeEach,
-  describe,
-  expect,
-  test,
-  vi,
-} from 'vitest';
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
+import { ConnectionClosedError } from '@viamrobotics/rpc';
 import { FakeTransportBuilder } from '@improbable-eng/grpc-web-fake-transport';
 import { RobotServiceClient } from './gen/robot/v1/robot_pb_service.esm';
+import { grpc } from '@improbable-eng/grpc-web';
 
 import SessionManager from './SessionManager';
 
@@ -19,9 +14,14 @@ const transport = new FakeTransportBuilder().build();
 let sm: SessionManager;
 
 const mockGetHeartBeatWindow = () => ({
-  getSeconds: () => 100,
-  getNanos: () => 100,
+  getSeconds: () => 1,
+  getNanos: () => 1,
 });
+
+// @ts-expect-error: 7006
+const mockHealthyHeartbeat = (_req, _md, cb) => {
+  cb(null, 'ok');
+};
 
 describe('SessionManager', () => {
   beforeEach(() => {
@@ -37,26 +37,149 @@ describe('SessionManager', () => {
     expect(sm.sessionID).eq('');
   });
 
-  test('start session', async () => {
-    const expectedSID = 'expected-sid';
+  test('start session when sessions are not supported', async () => {
+    RobotServiceClient.prototype.startSession = vi
+      .fn()
+      .mockImplementation((_req, _md, cb) => {
+        cb({ code: grpc.Code.Unimplemented }, null);
+      });
 
+    const metadata = await sm.getSessionMetadata();
+    expect(metadata.headersMap).toStrictEqual({});
+    expect(sm.sessionID).eq('');
+  });
+
+  test('start session without a response', async () => {
+    RobotServiceClient.prototype.startSession = vi
+      .fn()
+      .mockImplementation((_req, _md, cb) => {
+        cb(null, null);
+      });
+
+    await expect(sm.getSessionMetadata()).rejects.toStrictEqual({
+      code: grpc.Code.Internal,
+      message: 'expected response to start session',
+      metadata: new grpc.Metadata(),
+    });
+    expect(sm.sessionID).eq('');
+  });
+
+  test('start session without receiving a heartbeat window', async () => {
     RobotServiceClient.prototype.startSession = vi
       .fn()
       .mockImplementation((_req, _md, cb) => {
         cb(null, {
+          getId: () => 'some-sid',
+          getHeartbeatWindow: () => null,
+        });
+      });
+
+    await expect(sm.getSessionMetadata()).rejects.toStrictEqual({
+      code: grpc.Code.Internal,
+      message: 'expected heartbeat window in response to start session',
+      metadata: new grpc.Metadata(),
+    });
+    expect(sm.sessionID).eq('');
+  });
+
+  test('start session successfully', async () => {
+    const expectedSID = 'expected-sid';
+
+    RobotServiceClient.prototype.startSession = vi
+      .fn()
+      .mockImplementationOnce((_req, _md, cb) => {
+        cb(null, {
           getId: () => expectedSID,
+          getHeartbeatWindow: mockGetHeartBeatWindow,
+        });
+      })
+      .mockImplementationOnce((_req, _md, cb) => {
+        cb(null, {
+          getId: () => 'another-sid',
           getHeartbeatWindow: mockGetHeartBeatWindow,
         });
       });
     RobotServiceClient.prototype.sendSessionHeartbeat = vi
       .fn()
-      .mockImplementation((_req, _md, cb) => {
-        cb(null, 'ok');
-      });
+      .mockImplementation(mockHealthyHeartbeat);
 
-    const metadata = await sm.getSessionMetadata();
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    expect(metadata.headersMap!['viam-sid']![0]).eq(expectedSID);
+    let metadata = await sm.getSessionMetadata();
+    expect(metadata.headersMap).toStrictEqual({ 'viam-sid': [expectedSID] });
     expect(sm.sessionID).eq(expectedSID);
+
+    metadata = await sm.getSessionMetadata();
+    expect(metadata.headersMap).toStrictEqual({ 'viam-sid': [expectedSID] });
+    expect(sm.sessionID).eq(expectedSID);
+  });
+
+  test('start session and reset', async () => {
+    const initialSID = 'sid1';
+    const afterResetSID = 'sid2';
+
+    RobotServiceClient.prototype.startSession = vi
+      .fn()
+      .mockImplementationOnce((_req, _md, cb) => {
+        cb(null, {
+          getId: () => initialSID,
+          getHeartbeatWindow: mockGetHeartBeatWindow,
+        });
+      })
+      .mockImplementationOnce((_req, _md, cb) => {
+        cb(null, {
+          getId: () => afterResetSID,
+          getHeartbeatWindow: mockGetHeartBeatWindow,
+        });
+      });
+    RobotServiceClient.prototype.sendSessionHeartbeat = vi
+      .fn()
+      .mockImplementation(mockHealthyHeartbeat);
+
+    let metadata = await sm.getSessionMetadata();
+    expect(metadata.headersMap).toStrictEqual({ 'viam-sid': [initialSID] });
+    expect(sm.sessionID).eq(initialSID);
+
+    sm.reset();
+
+    metadata = await sm.getSessionMetadata();
+    expect(metadata.headersMap).toStrictEqual({ 'viam-sid': [afterResetSID] });
+    expect(sm.sessionID).eq(afterResetSID);
+  });
+
+  test('start session but heartbeat detects closed connection', async () => {
+    const initialSID = 'sid1';
+    const afterResetSID = 'sid2';
+
+    const reset = vi.spyOn(sm, 'reset');
+
+    RobotServiceClient.prototype.startSession = vi
+      .fn()
+      .mockImplementationOnce((_req, _md, cb) => {
+        cb(null, {
+          getId: () => initialSID,
+          getHeartbeatWindow: mockGetHeartBeatWindow,
+        });
+      })
+      .mockImplementationOnce((_req, _md, cb) => {
+        cb(null, {
+          getId: () => afterResetSID,
+          getHeartbeatWindow: mockGetHeartBeatWindow,
+        });
+      });
+    RobotServiceClient.prototype.sendSessionHeartbeat = vi
+      .fn()
+      .mockImplementationOnce((_req, _md, cb) => {
+        cb(new ConnectionClosedError('closed'), null);
+      })
+      .mockImplementation(mockHealthyHeartbeat);
+
+    let metadata = await sm.getSessionMetadata();
+    expect(metadata.headersMap).toStrictEqual({ 'viam-sid': [initialSID] });
+    expect(sm.sessionID).eq(initialSID);
+
+    expect(reset).toBeCalled();
+
+    metadata = await sm.getSessionMetadata();
+    expect(metadata.headersMap).toStrictEqual({ 'viam-sid': [afterResetSID] });
+    expect(sm.sessionID).eq(afterResetSID);
   });
 });
