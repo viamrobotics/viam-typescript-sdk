991a5a81c80d3a23897b684d341aefe0610cc22f
Maxim Pertsov <maxim@viam.com>
2023-02-13 14:40:28 -0500

    wip: teledemo

--- examples/teleop/src/index.js
+++ examples/teleop/src/index.js
@@ -2,47 +2,54 @@ import { Elm } from './Main.elm';
 import { Client, BaseClient, MotorClient } from '@viamrobotics/sdk';
 
 async function connect() {
-  // // You can remove this block entirely if your robot is not authenticated.
-  // // Otherwise, replace with an actual secret.
-  // const secret = import.meta.env.VITE_SECRET;
-  // const creds = {
-  //   payload: secret,
-  //   type: 'robot-location-secret',
-  // };
+  // You can remove this block entirely if your robot is not authenticated.
+  // Otherwise, replace with an actual secret.
+  const secret = import.meta.env.VITE_SECRET;
+  const creds = {
+    payload: secret,
+    type: 'robot-location-secret',
+  };
 
   // Replace with the host of your actual robot running Viam.
   const host = import.meta.env.VITE_HOST;
   const client = new Client(host);
 
   // Omit `creds` if your robot is not authenticated.
-  // await client.connect(undefined, creds);
-  await client.connect();
+  await client.connect(undefined, creds);
 
   return client;
 }
 
-const client = await connect();
-const base = new BaseClient(client, 'base1');
-const m1 = new MotorClient(client, 'motor1');
+// Connect and setup app
 
-const app = Elm.Main.init({
-  node: document.getElementById('main'),
-  flags: {},
-});
+connect()
+  .then((client) => {
+    const base = new BaseClient(client, 'viam_base');
+    const m1 = new MotorClient(client, 'left');
 
-app.ports.sendGetPosition.subscribe(async () => {
-  const position = await m1.getPosition();
-  app.ports.recvGetPosition.send(position);
-});
+    const app = Elm.Main.init({
+      node: document.getElementById('main'),
+      flags: {},
+    });
 
-app.ports.sendMotorGoFor.subscribe(async ({ rpm, revs }) => {
-  await m1.goFor(rpm, revs);
-  const position = await m1.getPosition();
-  app.ports.recvGetPosition.send(position);
-});
+    app.ports.sendGetPosition.subscribe(async () => {
+      const position = await m1.getPosition();
+      app.ports.recvGetPosition.send(position);
+    });
 
-app.ports.sendBaseMoveStraight.subscribe(async ({ dist, speed }) => {
-  await base.moveStraight(dist, speed);
-  const position = await m1.getPosition();
-  app.ports.recvGetPosition.send(position);
-});
+    app.ports.sendMotorGoFor.subscribe(async ({ rpm, revs }) => {
+      await m1.goFor(rpm, revs);
+      const position = await m1.getPosition();
+      app.ports.recvGetPosition.send(position);
+    });
+
+    app.ports.sendBaseMoveStraight.subscribe(async ({ dist, speed }) => {
+      await base.moveStraight(dist, speed);
+      const position = await m1.getPosition();
+      app.ports.recvGetPosition.send(position);
+    });
+  })
+  .catch((err) => {
+    console.error('something went wrong');
+    console.error(err);
+  });
