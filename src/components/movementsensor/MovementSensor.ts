28f0c53af6075de65bc1e0af4fc01a10f682ae48
Maxim Pertsov <maxim@viam.com>
2023-02-23 13:06:20 -0500

    define movementsensor interface

---
+++ src/components/movementsensor/MovementSensor.ts
@@ -0,0 +1,27 @@
+import type { Extra, Orientation, Vector3D } from '../../types';
+
+export interface Position {
+  latitude: number;
+  longitude: number;
+  altitudeMM: number;
+}
+
+export interface Properties {
+  linearVelocitySupported: boolean;
+  angularVelocitySupported: boolean;
+  orientationSupported: boolean;
+  positionSupported: boolean;
+  compassHeadingSupported: boolean;
+  linearAccelerationSupported: boolean;
+}
+
+export interface MovementSensor {
+  getLinearVelocity(extra?: Extra): Promise<Vector3D>;
+  getAngularVelocity(extra?: Extra): Promise<Vector3D>;
+  getCompassHeading(extra?: Extra): Promise<number>;
+  getOrientation(extra?: Extra): Promise<Orientation>;
+  getPosition(extra?: Extra): Promise<Position>;
+  getProperties(extra?: Extra): Promise<Properties>;
+  getAccuracy(extra?: Extra): Promise<number>;
+  getLinearAcceleration(extra?: Extra): Promise<Vector3D>;
+}
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Extra = Map<string, any>;
 
+export interface Vector3D {
+  x: number;
+  y: number;
+  z: number;
+}
+
+export interface Orientation {
+  ox: number;
+  oy: number;
+  oz: number;
+  theta: number;
+}
+
 export interface Options {
   requestLogger?: (req: unknown) => void;
 }
