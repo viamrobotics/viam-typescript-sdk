d06be52ec3045e931270d2bf2962a79afbfd20b1
oliviamiller <106617921+oliviamiller@users.noreply.github.com>
2023-02-28 11:15:44 -0500

    Changed methods to lowercase

--- src/components/arm/Arm.ts
+++ src/components/arm/Arm.ts
@@ -6,7 +6,7 @@ import type { Extra, Pose } from '../../types';
 
 export interface Arm {
   /** Get the position of the end of the arm expressed as a pose */
-  GetEndPosition: (extra?: Extra) => Promise<Pose>;
+  getEndPosition: (extra?: Extra) => Promise<Pose>;
 
   /**
    * Move the end of the arm to the pose, avoiding obstacles in the worldstate
@@ -14,9 +14,9 @@ export interface Arm {
    * @param pose - The destination pose for the arm
    * @param world - The obstacles for the arm to avoid
    */
-  MoveToPosition: (
+  moveToPosition: (
     pose: Pose,
-    world: WorldState,
+    world?: WorldState,
     extra?: Extra
   ) => Promise<void>;
 
@@ -26,17 +26,18 @@ export interface Arm {
    *
    * @param jointPositionsList - List of angles (0-360) to move each joint to
    */
-  MoveToJointPositions: (
+  moveToJointPositions: (
     jointPositionsList: number[],
     extra?: Extra
   ) => Promise<void>;
 
   /** Gets the current position of each joint */
-  GetJointPositions: (extra: Extra) => Promise<JointPositions>;
+  getJointPositions: (extra: Extra) => Promise<JointPositions>;
 
   /** Stops the motion of the arm */
-  Stop: (extra: Extra) => Promise<void>;
+  stop: (extra: Extra) => Promise<void>;
 
   /** Get if the arm is currently moving */
-  IsMoving: () => Promise<boolean>;
+  isMoving: () => Promise<boolean>;
 }
+
