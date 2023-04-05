c1b8eddbb010c630bef35e84c2d7f78129c56681
Maxim Pertsov <maxim@viam.com>
2023-04-05 09:02:51 -0400

    cleanup exports

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,6 @@
-import { RobotClient as Client } from './robot';
-import { version } from '../package.json';
+export { version } from '../package.json';
 
-import armApi from './gen/component/arm/v1/arm_pb';
-import baseApi from './gen/component/base/v1/base_pb';
-import boardApi from './gen/component/board/v1/board_pb';
-import cameraApi from './gen/component/camera/v1/camera_pb';
-import commonApi from './gen/common/v1/common_pb';
-import gantryApi from './gen/component/gantry/v1/gantry_pb';
-import genericApi from './gen/component/generic/v1/generic_pb';
-import gripperApi from './gen/component/gripper/v1/gripper_pb';
-import inputControllerApi from './gen/component/inputcontroller/v1/input_controller_pb';
-import motionApi from './gen/service/motion/v1/motion_pb';
-import motorApi from './gen/component/motor/v1/motor_pb';
-import movementSensorApi from './gen/component/movementsensor/v1/movementsensor_pb';
-import navigationApi from './gen/service/navigation/v1/navigation_pb';
-import robotApi from './gen/robot/v1/robot_pb';
-import sensorApi from './gen/component/sensor/v1/sensor_pb';
-import sensorsApi from './gen/service/sensors/v1/sensors_pb';
-import servoApi from './gen/component/servo/v1/servo_pb';
-import slamApi from './gen/service/slam/v1/slam_pb';
-import streamApi from './gen/proto/stream/v1/stream_pb';
-import visionApi from './gen/service/vision/v1/vision_pb';
+// Wrappers
 
 export {
   type Robot,
@@ -29,6 +9,8 @@ export {
   RobotClient,
   createRobotClient,
 } from './robot';
+export { RobotClient as Client } from './robot';
+
 export { type Arm, ArmClient } from './components/arm';
 export { type Base, BaseClient } from './components/base';
 export { type Board, BoardClient } from './components/board';
@@ -41,30 +23,28 @@ export {
 export { type Sensor, SensorClient } from './components/sensor';
 export { type Stream, StreamClient } from './extra/stream';
 
-export {
-  commonApi,
-  armApi,
-  baseApi,
-  boardApi,
-  cameraApi,
-  gantryApi,
-  genericApi,
-  gripperApi,
-  inputControllerApi,
-  motorApi,
-  motionApi,
-  movementSensorApi,
-  servoApi,
-  navigationApi,
-  robotApi,
-  sensorApi,
-  sensorsApi,
-  slamApi,
-  visionApi,
-  streamApi,
-  Client,
-  version,
-};
+// Raw Protobufs
+
+export { default as armApi } from './gen/component/arm/v1/arm_pb';
+export { default as baseApi } from './gen/component/base/v1/base_pb';
+export { default as boardApi } from './gen/component/board/v1/board_pb';
+export { default as cameraApi } from './gen/component/camera/v1/camera_pb';
+export { default as commonApi } from './gen/common/v1/common_pb';
+export { default as gantryApi } from './gen/component/gantry/v1/gantry_pb';
+export { default as genericApi } from './gen/component/generic/v1/generic_pb';
+export { default as gripperApi } from './gen/component/gripper/v1/gripper_pb';
+export { default as inputControllerApi } from './gen/component/inputcontroller/v1/input_controller_pb';
+export { default as motionApi } from './gen/service/motion/v1/motion_pb';
+export { default as motorApi } from './gen/component/motor/v1/motor_pb';
+export { default as movementSensorApi } from './gen/component/movementsensor/v1/movementsensor_pb';
+export { default as navigationApi } from './gen/service/navigation/v1/navigation_pb';
+export { default as robotApi } from './gen/robot/v1/robot_pb';
+export { default as sensorApi } from './gen/component/sensor/v1/sensor_pb';
+export { default as sensorsApi } from './gen/service/sensors/v1/sensors_pb';
+export { default as servoApi } from './gen/component/servo/v1/servo_pb';
+export { default as slamApi } from './gen/service/slam/v1/slam_pb';
+export { default as streamApi } from './gen/proto/stream/v1/stream_pb';
+export { default as visionApi } from './gen/service/vision/v1/vision_pb';
 
 // It doesn't matter which ServiceError we export really.
 export type {
