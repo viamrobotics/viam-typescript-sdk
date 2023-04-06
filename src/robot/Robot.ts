67b63f4b9d84abe7972694abf8d2d0dc4e4568f9
Maxim Pertsov <maxim@viam.com>
2023-04-06 16:09:34 -0400

    remove private remarks

--- src/robot/Robot.ts
+++ src/robot/Robot.ts
@@ -11,9 +11,6 @@ export interface Robot {
   /**
    * Get the list of operations currently running on the robot.
    *
-   * @privateRemarks
-   * TODO: this function should return an more idiomatic type instead of just
-   * passing along a proto type.
    * @group Operations
    * @alpha
    */
@@ -52,9 +49,6 @@ export interface Robot {
   /**
    * Get the configuration of the frame system of a given robot.
    *
-   * @privateRemarks
-   * TODO: this function should return an more idiomatic type instead of just
-   * passing along a proto type.
    * @group Frame System
    * @alpha
    */
@@ -64,9 +58,6 @@ export interface Robot {
    * Transform a given source Pose from the reference frame to a new specified
    * destination which is a reference frame.
    *
-   * @privateRemarks
-   * TODO: this function should return an more idiomatic type instead of just
-   * passing along a proto type.
    * @param query - The pose that should be transformed
    * @param destination - The name of the reference frame to transform the given
    * @param supplementalTransforms - Pose information on any additional
@@ -105,9 +96,6 @@ export interface Robot {
   /**
    * Get the list of discovered component configurations.
    *
-   * @privateRemarks
-   * TODO: this function should return an more idiomatic type instead of just
-   * passing along a proto type.
    * @param queries - The list of component models to discovery.
    * @group Discovery
    * @alpha
