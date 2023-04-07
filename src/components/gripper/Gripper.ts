afff670828801300aff894ac3a727cf5496ac3a2
Maxim Pertsov <maxim@viam.com>
2023-04-07 11:54:26 -0400

    fix docstrings

--- src/components/gripper/Gripper.ts
+++ src/components/gripper/Gripper.ts
@@ -2,15 +2,15 @@ import type { Extra } from '../../types';
 
 /** Represents a physical robotic gripper. */
 export interface Gripper {
-  /* Open a gripper of the underlying robot. */
+  /** Open a gripper of the underlying robot. */
   open: (extra?: Extra) => Promise<void>;
 
-  /* Request a gripper of the underlying robot to grab. */
+  /** Request a gripper of the underlying robot to grab. */
   grab: (extra?: Extra) => Promise<void>;
 
-  /* Stop a robot's gripper. */
+  /** Stop a robot's gripper. */
   stop: (extra?: Extra) => Promise<void>;
 
-  /* Report if a component is in motion. */
+  /** Report if a component is in motion. */
   isMoving: () => Promise<boolean>;
 }
