17020a0dd9c1d53d46be8723c637d9c881be8cef
Michael Cousins <michael.cousins@viam.com>
2023-04-11 17:03:45 -0400

    fixup: remove unused stuff

--- examples/teleop-react/vite.config.ts
+++ examples/teleop-react/vite.config.ts
@@ -1,13 +1,7 @@
-/// <reference types="vitest" />
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-  },
 });
