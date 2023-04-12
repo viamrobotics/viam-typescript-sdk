916f86e04c1e060029556c7be4c81446b03c39e4
martha-johnston <106617924+martha-johnston@users.noreply.github.com>
2023-04-12 15:50:08 -0400

    RSDK-2480: Encoder API TypeScript SDK changes (#58)

---
+++ src/components/encoder/Encoder.ts
@@ -0,0 +1,25 @@
+import type { Extra, PositionType } from '../../types';
+import pb from '../../gen/component/encoder/v1/encoder_pb';
+
+export type EncoderProperties = pb.GetPropertiesResponse.AsObject;
+
+/** Represents a physical encoder. */
+export interface Encoder {
+  /** Set the current position of the encoder as the new zero position. */
+  resetPosition(extra?: Extra): Promise<void>;
+
+  /** Return the encoder's properties. */
+  getProperties(extra?: Extra): Promise<EncoderProperties>;
+
+  /**
+   * Return the current position either in relative units (ticks away from a
+   * zero position) or absolute units (degrees along a circle).
+   *
+   * @param positionType - The type of position the encoder returns (ticks or
+   *   degrees)
+   */
+  getPosition(
+    positionType?: PositionType,
+    extra?: Extra
+  ): Promise<readonly [number, PositionType]>;
+}
