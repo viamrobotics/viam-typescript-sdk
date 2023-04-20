3e894297fdb3288c1f1deabe6c962e474a4edb27
Maxim Pertsov <maxim@viam.com>
2023-04-20 17:00:34 -0400

    use POJO instead of enum

--- src/types.ts
+++ src/types.ts
@@ -24,12 +24,6 @@ export interface Options {
   requestLogger?: (req: unknown) => void;
 }
 
-export enum PositionType {
-  POSITION_TYPE_UNSPECIFIED = 0,
-  POSITION_TYPE_TICKS_COUNT = 1,
-  POSITION_TYPE_ANGLE_DEGREES = 2,
-}
-
 // Common Protobuf Types
 
 export type ResourceName = common.ResourceName.AsObject;
