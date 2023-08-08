a59621848cc645bfedc5852e500723c3f52da878
purplenicole730 <jihyun.nicole.jung@gmail.com>
2023-08-07 22:43:29 -0400

    add two describe blocks to group tests

--- src/services/slam/client.test.ts
+++ src/services/slam/client.test.ts
@@ -1,6 +1,6 @@
 // @vitest-environment happy-dom
 
-import { afterEach, beforeEach, expect, test, vi } from 'vitest';
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
 import { SLAMServiceClient } from '../../gen/service/slam/v1/slam_pb_service';
 import { type ResponseStream } from '../../gen/robot/v1/robot_pb_service';
 import { RobotClient } from '../../robot';
@@ -65,90 +65,94 @@ afterEach(() => {
   testInternalStream = undefined;
 });
 
-test('get point cloud map', () => {
-  const promise = slam.getPointCloudMap();
+describe('getPointCloudMap tests', () => {
+  test('get point cloud map', () => {
+    const promise = slam.getPointCloudMap();
 
-  const response1 = new GetPointCloudMapResponse();
-  const chunk1 = new Uint8Array([4, 13]);
-  response1.setPointCloudPcdChunk(chunk1);
-  testPcdStream?.emit('data', response1);
+    const response1 = new GetPointCloudMapResponse();
+    const chunk1 = new Uint8Array([4, 13]);
+    response1.setPointCloudPcdChunk(chunk1);
+    testPcdStream?.emit('data', response1);
 
-  const response2 = new GetPointCloudMapResponse();
-  const chunk2 = new Uint8Array([16, 25]);
-  response2.setPointCloudPcdChunk(chunk2);
-  testPcdStream?.emit('data', response2);
-  testPcdStream?.emit('end', { code: 0 });
+    const response2 = new GetPointCloudMapResponse();
+    const chunk2 = new Uint8Array([16, 25]);
+    response2.setPointCloudPcdChunk(chunk2);
+    testPcdStream?.emit('data', response2);
+    testPcdStream?.emit('end', { code: 0 });
 
-  expect(promise).resolves.toStrictEqual([chunk1, chunk2]);
+    expect(promise).resolves.toStrictEqual([chunk1, chunk2]);
+  });
+
+  test('end getPcdMap stream with wrong code', () => {
+    const error = { code: 1, details: 'fake', metadata: undefined };
+
+    const promise1 = slam.getPointCloudMap();
+    // @ts-expect-error We need to pass "undefined" as a second argument here, but it gets removed by the linter since omitting an argument is equivalent to passing in "undefined"
+    testPcdStream?.emit('end');
+    expect(promise1).rejects.toStrictEqual({
+      message: 'Stream ended without a status code',
+    });
+
+    const promise2 = slam.getPointCloudMap();
+    testPcdStream?.emit('end', error);
+    expect(promise2).rejects.toStrictEqual({
+      code: 1,
+      message: 'fake',
+      metadata: undefined,
+    });
+
+    const promise3 = slam.getPointCloudMap();
+    testPcdStream?.emit('status', error);
+    expect(promise3).rejects.toStrictEqual({
+      code: 1,
+      message: 'fake',
+      metadata: undefined,
+    });
+  });
 });
 
-test('end getPcdMap stream with wrong code', () => {
-  const error = { code: 1, details: 'fake', metadata: undefined };
+describe('getInternalState tests', () => {
+  test('get internal state', () => {
+    const promise = slam.getInternalState();
 
-  const promise1 = slam.getPointCloudMap();
-  // @ts-expect-error We need to pass "undefined" as a second argument here, but it gets removed by the linter since omitted an argument is equivalent to passing in "undefined"
-  testPcdStream?.emit('end');
-  expect(promise1).rejects.toStrictEqual({
-    message: 'Stream ended without a status code',
+    const response1 = new GetInternalStateResponse();
+    const chunk1 = new Uint8Array([4, 13]);
+    response1.setInternalStateChunk(chunk1);
+    testInternalStream?.emit('data', response1);
+
+    const response2 = new GetInternalStateResponse();
+    const chunk2 = new Uint8Array([16, 25]);
+    response2.setInternalStateChunk(chunk2);
+    testInternalStream?.emit('data', response2);
+    testInternalStream?.emit('end', { code: 0 });
+
+    expect(promise).resolves.toStrictEqual([chunk1, chunk2]);
   });
 
-  const promise2 = slam.getPointCloudMap();
-  testPcdStream?.emit('end', error);
-  expect(promise2).rejects.toStrictEqual({
-    code: 1,
-    message: 'fake',
-    metadata: undefined,
-  });
+  test('end getInternalState stream with wrong code', () => {
+    const error = { code: 1, details: 'fake', metadata: undefined };
 
-  const promise3 = slam.getPointCloudMap();
-  testPcdStream?.emit('status', error);
-  expect(promise3).rejects.toStrictEqual({
-    code: 1,
-    message: 'fake',
-    metadata: undefined,
-  });
-});
+    const promise1 = slam.getInternalState();
+    // @ts-expect-error We need to pass "undefined" as a second argument here, but it gets removed by the linter since omitting an argument is equivalent to passing in "undefined"
+    testInternalStream?.emit('end');
+    expect(promise1).rejects.toStrictEqual({
+      message: 'Stream ended without a status code',
+    });
 
-test('get internal state', () => {
-  const promise = slam.getInternalState();
+    const promise2 = slam.getInternalState();
+    testInternalStream?.emit('end', error);
+    expect(promise2).rejects.toStrictEqual({
+      code: 1,
+      message: 'fake',
+      metadata: undefined,
+    });
 
-  const response1 = new GetInternalStateResponse();
-  const chunk1 = new Uint8Array([4, 13]);
-  response1.setInternalStateChunk(chunk1);
-  testInternalStream?.emit('data', response1);
-
-  const response2 = new GetInternalStateResponse();
-  const chunk2 = new Uint8Array([16, 25]);
-  response2.setInternalStateChunk(chunk2);
-  testInternalStream?.emit('data', response2);
-  testInternalStream?.emit('end', { code: 0 });
-
-  expect(promise).resolves.toStrictEqual([chunk1, chunk2]);
-});
-
-test('end getInternalState stream with wrong code', () => {
-  const error = { code: 1, details: 'fake', metadata: undefined };
-
-  const promise1 = slam.getInternalState();
-  // @ts-expect-error We need to pass "undefined" as a second argument here, but it gets removed by the linter since omitted an argument is equivalent to passing in "undefined"
-  testInternalStream?.emit('end');
-  expect(promise1).rejects.toStrictEqual({
-    message: 'Stream ended without a status code',
-  });
-
-  const promise2 = slam.getInternalState();
-  testInternalStream?.emit('end', error);
-  expect(promise2).rejects.toStrictEqual({
-    code: 1,
-    message: 'fake',
-    metadata: undefined,
-  });
-
-  const promise3 = slam.getInternalState();
-  testInternalStream?.emit('status', error);
-  expect(promise3).rejects.toStrictEqual({
-    code: 1,
-    message: 'fake',
-    metadata: undefined,
+    const promise3 = slam.getInternalState();
+    testInternalStream?.emit('status', error);
+    expect(promise3).rejects.toStrictEqual({
+      code: 1,
+      message: 'fake',
+      metadata: undefined,
+    });
   });
 });
