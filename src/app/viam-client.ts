4716eae04944d23c08af4a4d4c38d5cda10043d8
purplenicole730 <jihyun.nicole.jung@gmail.com>
2023-08-30 16:56:19 -0400

    add viam client

---
+++ src/app/viam-client.ts
@@ -0,0 +1,36 @@
+import { grpc } from '@improbable-eng/grpc-web';
+import { type DialOptions } from '@viamrobotics/rpc/src/dial';
+import { DataServiceClient } from '../gen/app/data/v1/data_pb_service';
+import { createViamTransportFactory } from '../robot/dial';
+
+export class ViamClient {
+  private serviceHost: string;
+  private dialOpts: DialOptions;
+  private dataServiceClient: DataServiceClient | undefined;
+  private transportFactory: grpc.TransportFactory | undefined;
+
+  constructor(serviceHost: string, dialOpts: DialOptions) {
+    this.serviceHost = serviceHost;
+    this.dialOpts = dialOpts;
+  }
+
+  public getTransportFactory = async () => {
+    return createViamTransportFactory(this.dialOpts);
+  };
+
+  get dataService() {
+    if (!this.dataServiceClient) {
+      throw new Error('not connected yet');
+    }
+    return this.dataServiceClient;
+  }
+
+  public async connect() {
+    this.transportFactory = await this.getTransportFactory();
+    const grpcOptions = { transport: this.transportFactory };
+    this.dataServiceClient = new DataServiceClient(
+      this.serviceHost,
+      grpcOptions
+    );
+  }
+}
