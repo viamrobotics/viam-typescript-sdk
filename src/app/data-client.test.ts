7aefe707b6e84bef1facf0a1e3e8746c7e8ac75c
purplenicole730 <jihyun.nicole.jung@gmail.com>
2023-09-25 17:11:52 -0400

    test tabulardatabyfilter

--- src/app/data-client.test.ts
+++ src/app/data-client.test.ts
@@ -1,11 +1,16 @@
 import { FakeTransportBuilder } from '@improbable-eng/grpc-web-fake-transport';
 import { Timestamp } from 'google-protobuf/google/protobuf/timestamp_pb';
-import { beforeEach, describe, expect, test } from 'vitest';
+import { Struct } from 'google-protobuf/google/protobuf/struct_pb';
+import { beforeEach, describe, expect, test, vi } from 'vitest';
 import {
   CaptureInterval,
   Filter,
+  TabularData,
+  TabularDataByFilterResponse,
   TagsFilter,
 } from '../gen/app/data/v1/data_pb';
+import { DataServiceClient } from '../gen/app/data/v1/data_pb_service';
+vi.mock('../gen/app/data/v1/data_pb_service');
 import { DataClient, type FilterOptions } from './data-client';
 
 const serviceHost = 'fakeServiceHost';
@@ -29,8 +34,48 @@ interval.setEnd(Timestamp.fromDate(new Date(2, 2, 2, 2, 2, 2)));
 const tagsFilter = new TagsFilter();
 tagsFilter.setTagsList(['testTag1', 'testTag2']);
 
+const tabularData1 = new TabularData();
+const data1 = { key: 'value1' };
+const struct1 = Struct.fromJavaScript(data1);
+tabularData1.setData(struct1);
+const tabularData2 = new TabularData();
+const data2 = { key: 'value2' };
+const struct2 = Struct.fromJavaScript(data2);
+tabularData2.setData(struct2);
+const tabularDataResponse = new TabularDataByFilterResponse();
+tabularDataResponse.setDataList([tabularData1, tabularData2]);
+
 beforeEach(() => {
+  DataServiceClient.prototype.tabularDataByFilter = vi
+    .fn()
+    .mockImplementationOnce((_req, _md, cb) => {
+      cb(null, {
+        getDataList: () => tabularDataResponse.getDataList(),
+        getLast: () => tabularDataResponse.getLast(),
+      });
+    })
+    .mockImplementation((_req, _md, cb) => {
+        cb(null, {
+            getDataList: () => []
+        })
+    });
   dataClient = new DataClient(serviceHost, { transport });
+});
+
+describe('tabularDataByFilter tests',  () => {
+  const filter: Filter = new Filter();
+  const dataList = tabularDataResponse.getDataList();
+  const dataArray: TabularData.AsObject[] = [];
+  dataArray.push(...dataList.map((data) => data.toObject()));
+
+  test('tabulardatabyfilter', async () => {
+    const promise = await dataClient.tabularDataByFilter(filter);
+    expect(promise.length).toEqual(2);
+    expect(promise[0].data.fieldsMap[0][0]).toEqual('key')
+    expect(promise[0].data.fieldsMap[0][1].stringValue).toEqual('value1')
+    expect(promise[1].data.fieldsMap[0][0]).toEqual('key')
+    expect(promise[1].data.fieldsMap[0][1].stringValue).toEqual('value2')
+  });
 });
 
 describe('createFilter', () => {
