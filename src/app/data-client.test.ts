7c626f1c62254c642afca12854ada5e807aa85c9
purplenicole730 <jihyun.nicole.jung@gmail.com>
2023-09-25 17:12:26 -0400

    clean up

--- src/app/data-client.test.ts
+++ src/app/data-client.test.ts
@@ -13,26 +13,10 @@ import { DataServiceClient } from '../gen/app/data/v1/data_pb_service';
 vi.mock('../gen/app/data/v1/data_pb_service');
 import { DataClient, type FilterOptions } from './data-client';
 
+
 const serviceHost = 'fakeServiceHost';
 const transport = new FakeTransportBuilder().build();
 let dataClient: DataClient;
-
-const componentName = 'testComponentName';
-const componentType = 'testComponentType';
-const method = 'testMethod';
-const robotName = 'testRobotName';
-const robotId = 'testRobotId';
-const partName = 'testPartName';
-const partId = 'testPartId';
-const locationsIdsList = ['testLocationId1', 'testLocationId2'];
-const organizationIdsList = ['testOrgId1', 'testOrgId2'];
-const mimeTypeList = ['testMimeType1', 'testMimeType2'];
-const bboxLabelsList = ['testBboxLabel1', 'testBboxLabel2'];
-const interval = new CaptureInterval();
-interval.setStart(Timestamp.fromDate(new Date(1, 1, 1, 1, 1, 1)));
-interval.setEnd(Timestamp.fromDate(new Date(2, 2, 2, 2, 2, 2)));
-const tagsFilter = new TagsFilter();
-tagsFilter.setTagsList(['testTag1', 'testTag2']);
 
 const tabularData1 = new TabularData();
 const data1 = { key: 'value1' };
@@ -80,53 +64,70 @@ describe('tabularDataByFilter tests',  () => {
 
 describe('createFilter', () => {
   let opts: FilterOptions;
-  let filter: Filter;
   let testFilter: Filter;
+  let actualFilter: Filter;
 
   test('create filter', () => {
     opts = { componentName: 'camera' };
-    filter = dataClient.createFilter(opts);
+    testFilter = dataClient.createFilter(opts);
 
-    const thing = new Filter();
-    thing.setComponentName('camera');
+    actualFilter = new Filter();
+    actualFilter.setComponentName('camera');
 
-    expect(filter).toEqual(thing);
+    expect(testFilter).toEqual(actualFilter);
   });
 
   test('create filter with all options', () => {
+    const componentName = 'testComponentName';
+    const componentType = 'testComponentType';
+    const method = 'testMethod';
+    const robotName = 'testRobotName';
+    const robotId = 'testRobotId';
+    const partName = 'testPartName';
+    const partId = 'testPartId';
+    const locationsIdsList = ['testLocationId1', 'testLocationId2'];
+    const organizationIdsList = ['testOrgId1', 'testOrgId2'];
+    const mimeTypeList = ['testMimeType1', 'testMimeType2'];
+    const bboxLabelsList = ['testBboxLabel1', 'testBboxLabel2'];
+    const interval = new CaptureInterval();
+    interval.setStart(Timestamp.fromDate(new Date(1, 1, 1, 1, 1, 1)));
+    interval.setEnd(Timestamp.fromDate(new Date(2, 2, 2, 2, 2, 2)));
+    const tagsFilter = new TagsFilter();
+    tagsFilter.setTagsList(['testTag1', 'testTag2']);
+
     opts = {
-      componentName: 'testComponentName',
-      componentType: 'testComponentType',
-      method: 'testMethod',
-      robotName: 'testRobotName',
-      robotId: 'testRobotId',
-      partName: 'testPartName',
-      partId: 'testPartId',
-      locationIdsList: ['testLocationId1', 'testLocationId2'],
-      organizationIdsList: ['testOrgId1', 'testOrgId2'],
-      mimeTypeList: ['testMimeType1', 'testMimeType2'],
-      bboxLabelsList: ['testBboxLabel1', 'testBboxLabel2'],
+      componentName: componentName,
+      componentType: componentType,
+      method: method,
+      robotName: robotName,
+      robotId: robotId,
+      partName: partName,
+      partId: partId,
+      locationIdsList: locationsIdsList,
+      organizationIdsList: organizationIdsList,
+      mimeTypeList: mimeTypeList,
+      bboxLabelsList: bboxLabelsList,
       startTime: new Date(1, 1, 1, 1, 1, 1),
       endTime: new Date(2, 2, 2, 2, 2, 2),
       tags: ['testTag1', 'testTag2'],
     };
-    filter = dataClient.createFilter(opts);
+    testFilter = dataClient.createFilter(opts);
 
-    testFilter = new Filter();
-    testFilter.setComponentName(componentName);
-    testFilter.setComponentType(componentType);
-    testFilter.setMethod(method);
-    testFilter.setRobotName(robotName);
-    testFilter.setRobotId(robotId);
-    testFilter.setPartName(partName);
-    testFilter.setPartId(partId);
-    testFilter.setLocationIdsList(locationsIdsList);
-    testFilter.setOrganizationIdsList(organizationIdsList);
-    testFilter.setMimeTypeList(mimeTypeList);
-    testFilter.setBboxLabelsList(bboxLabelsList);
-    testFilter.setInterval(interval);
-    testFilter.setTagsFilter(tagsFilter);
+    actualFilter = new Filter();
+    actualFilter.setComponentName(componentName);
+    actualFilter.setComponentType(componentType);
+    actualFilter.setMethod(method);
+    actualFilter.setRobotName(robotName);
+    actualFilter.setRobotId(robotId);
+    actualFilter.setPartName(partName);
+    actualFilter.setPartId(partId);
+    actualFilter.setLocationIdsList(locationsIdsList);
+    actualFilter.setOrganizationIdsList(organizationIdsList);
+    actualFilter.setMimeTypeList(mimeTypeList);
+    actualFilter.setBboxLabelsList(bboxLabelsList);
+    actualFilter.setInterval(interval);
+    actualFilter.setTagsFilter(tagsFilter);
 
-    expect(filter).toEqual(testFilter);
+    expect(testFilter).toEqual(actualFilter);
   });
 });
