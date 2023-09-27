3c6573e26eb31ddd09f496304f01fb5b0b22841c
purplenicole730 <jihyun.nicole.jung@gmail.com>
2023-09-27 14:01:49 -0400

    undo changes in example

--- examples/connect-app/src/main.ts
+++ examples/connect-app/src/main.ts
@@ -3,12 +3,12 @@ import * as VIAM from '@viamrobotics/sdk';
 
 async function connect(): Promise<VIAM.ViamClient> {
   const credential = {
-    payload: '2gi0e4bndmuxep6eto0rsrx1hv76peo7',
+    payload: '<API-KEY>',
     type: 'api-key',
   };
 
   const dialOpts: DialOptions = {
-    authEntity: '7424310b-6b42-466e-b4ad-2c709eaeea42',
+    authEntity: '<API-KEY-ID>',
     credentials: credential,
   };
 
@@ -22,10 +22,7 @@ const button = <HTMLButtonElement>document.getElementById('main-button');
 
 async function run(client: VIAM.ViamClient) {
   // A filter is an optional tool to filter out which data comes back.
-  const opts: VIAM.FilterOptions = {
-    locationIdsList: ['b3g79ptyug'],
-    mimeTypeList: ['image/jpeg'],
-  };
+  const opts: VIAM.FilterOptions = { componentType: 'camera' };
   const filter = client.dataClient.createFilter(opts);
 
   try {
@@ -33,19 +30,7 @@ async function run(client: VIAM.ViamClient) {
     const textElement = <HTMLParagraphElement>document.getElementById('text');
     textElement.innerHTML = 'waiting for data...';
 
-    const binaryId: VIAM.BinaryID = {
-      fileId:
-        'KSuK20sVumkpMqVZx7gAj99Qqt9nRsHrbrYeQYgnrEQ1QpugXhu9G1ORgs0Y0DLo',
-      organizationId: 'e76d1b3b-0468-4efd-bb7f-fb1d2b352fcb',
-      locationId: 'b3g79ptyug',
-    };
-
-    const dataList = await client.dataClient.binaryDataByIds([binaryId]);
-    const dataList2 = await client.dataClient.binaryDataByFilter(filter);
-    // const text = JSON.stringify(dataList, null, 2) + JSON.stringify(dataList2, null, 2);
-    console.log(dataList);
-    console.log(dataList2);
-    textElement.innerHTML = JSON.stringify(dataList, null, 2);
+    const dataList = await client.dataClient.tabularDataByFilter(filter);
   } finally {
     button.disabled = false;
   }
