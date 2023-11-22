b3f620de947079bdd8734df33eef37c9aab63794
Maxim Pertsov <maxim@viam.com>
2023-11-22 10:30:57 -0500

    update example to fetch data by SQL

--- examples/connect-app/src/main.ts
+++ examples/connect-app/src/main.ts
@@ -1,5 +1,6 @@
 import * as VIAM from '@viamrobotics/sdk';
 
+const ORG_ID = import.meta.env.VITE_ORG_ID;
 const API_KEY_ID = import.meta.env.VITE_API_KEY_ID;
 const API_KEY_SECRET = import.meta.env.VITE_API_KEY_SECRET;
 
@@ -20,16 +21,15 @@ async function connect(): Promise<VIAM.ViamClient> {
 const button = <HTMLButtonElement>document.getElementById('main-button');
 
 async function run(client: VIAM.ViamClient) {
-  // A filter is an optional tool to filter out which data comes back.
-  const opts: VIAM.FilterOptions = { componentType: 'camera' };
-  const filter = client.dataClient.createFilter(opts);
-
   try {
     button.disabled = true;
     const textElement = <HTMLParagraphElement>document.getElementById('text');
     textElement.innerHTML = 'waiting for data...';
 
-    const dataList = await client.dataClient.tabularDataByFilter(filter);
+    const dataList = await client.dataClient.tabularDataBySQL(
+      ORG_ID,
+      'select * from readings limit 5'
+    );
     textElement.innerHTML = JSON.stringify(dataList, null, 2);
   } finally {
     button.disabled = false;
