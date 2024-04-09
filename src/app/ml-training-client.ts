a9daa507f50d3d4fe38068926e7041ccac329828
purplenicole730 <jihyun.nicole.jung@gmail.com>
2024-04-09 13:18:29 -0400

    add ml training wrappers

---
+++ src/app/ml-training-client.ts
@@ -0,0 +1,94 @@
+import { type RpcOptions } from '@improbable-eng/grpc-web/dist/typings/client';
+import { MLTrainingServiceClient } from '../gen/app/mltraining/v1/ml_training_pb_service';
+import pb from '../gen/app/mltraining/v1/ml_training_pb';
+import { promisify } from '../utils';
+
+export type ModelTypeMap = pb.ModelTypeMap;
+export type TrainingStatusMap = pb.TrainingStatusMap;
+
+export class MLTrainingClient {
+  private service: MLTrainingServiceClient;
+
+  constructor(serviceHost: string, grpcOptions: RpcOptions) {
+    this.service = new MLTrainingServiceClient(serviceHost, grpcOptions);
+  }
+
+  async submitTrainingJob(
+    datasetId: string,
+    orgId: string,
+    modelName: string,
+    modelVersion: string,
+    modelType: ModelTypeMap[keyof ModelTypeMap],
+    tagsList: string[]
+  ) {
+    const { service } = this;
+
+    const req = new pb.SubmitTrainingJobRequest();
+    req.setDatasetId(datasetId);
+    req.setOrganizationId(orgId);
+    req.setModelName(modelName);
+    req.setModelVersion(modelVersion);
+    req.setModelType(modelType);
+    req.setTagsList(tagsList);
+
+    const response = await promisify<
+      pb.SubmitTrainingJobRequest,
+      pb.SubmitTrainingJobResponse
+    >(service.submitTrainingJob.bind(service), req);
+    return response.getId();
+  }
+
+  async getTrainingJobRequest(id: string) {
+    const { service } = this;
+
+    const req = new pb.GetTrainingJobRequest();
+    req.setId(id);
+
+    const response = await promisify<
+      pb.GetTrainingJobRequest,
+      pb.GetTrainingJobResponse
+    >(service.getTrainingJob.bind(service), req);
+    return response.getMetadata();
+  }
+
+  async listTrainingJobs(
+    orgId: string,
+    status: TrainingStatusMap[keyof TrainingStatusMap]
+  ) {
+    const { service } = this;
+
+    const req = new pb.ListTrainingJobsRequest();
+    req.setOrganizationId(orgId);
+    req.setStatus(status);
+
+    const response = await promisify<
+      pb.ListTrainingJobsRequest,
+      pb.ListTrainingJobsResponse
+    >(service.listTrainingJobs.bind(service), req);
+    return response.toObject().jobsList;
+  }
+
+  async cancelTrainingJob(id: string) {
+    const { service } = this;
+
+    const req = new pb.CancelTrainingJobRequest();
+    req.setId(id);
+
+    await promisify<pb.CancelTrainingJobRequest, pb.CancelTrainingJobResponse>(
+      service.cancelTrainingJob.bind(service),
+      req
+    );
+  }
+
+  async deleteCompletedTrainingJob(id: string) {
+    const { service } = this;
+
+    const req = new pb.DeleteCompletedTrainingJobRequest();
+    req.setId(id);
+
+    await promisify<
+      pb.DeleteCompletedTrainingJobRequest,
+      pb.DeleteCompletedTrainingJobResponse
+    >(service.deleteCompletedTrainingJob.bind(service), req);
+  }
+}
