4b0835184f2bc2d37c12347d06ec654b9b7be896
Nick Franczak <nicholasgfranczak@gmail.com>
2024-05-14 16:30:51 -0400

    remove un-needed lint

--- src/services/motion/client.test.ts
+++ src/services/motion/client.test.ts
@@ -51,7 +51,6 @@ describe('moveOnGlobe', () => {
       name: 'myMovementsensor',
     };
     const expectedMotionConfiguration = undefined;
-    // const expectedBoundingRegionsList = 
     const expectedExtra = {};
     const mock = vi
       .fn()
