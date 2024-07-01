c226f2d9551fe9573229809001fef4554df11157
Ethan Look <ethan.look@viam.com>
2024-07-01 10:11:06 -0400

    fixup: lint

--- src/components/inputcontroller.ts
+++ src/components/inputcontroller.ts
@@ -1,2 +1,5 @@
-export type { InputController, InputControllerEvent } from './input-controller/input-controller';
+export type {
+  InputController,
+  InputControllerEvent,
+} from './input-controller/input-controller';
 export { InputControllerClient } from './input-controller/client';
