cc5318b9aec7ed0447f23784762fbbac835c5916
Eric Daniels <eric@erdaniels.com>
2024-10-08 17:21:20 -0400

    Add simple e2e test (#382)

--- vite.config.ts
+++ vite.config.ts
@@ -29,5 +29,13 @@ export default defineConfig({
   },
   test: {
     mockReset: true,
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+      '**/e2e/**',
+    ],
   },
 });
