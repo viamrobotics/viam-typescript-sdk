80dc61e0a5daea7a04b990c1159026c08b4df46a
DTCurrie <devintcurrie@gmail.com>
2025-02-12 15:11:26 -0500

    export types we use

--- src/services/ml-model/index.ts
+++ src/services/ml-model/index.ts
@@ -1,2 +1,7 @@
 export type { MLModel } from './ml-model';
 export { MLModelClient } from './client';
+export {
+  FlatTensors,
+  InferResponse,
+  MetadataResponse,
+} from '../../gen/service/mlmodel/v1/mlmodel_pb';
