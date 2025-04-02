28a37b6b50e4e4eecbe6bdec42fe1e7159ef1377
JessamyT <jessamy.taylor@gmail.com>
2025-04-02 15:44:33 -0700

    DOCS-3820: Add button and switch examples

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -3,6 +3,17 @@ import type { Resource } from '../../types';
 
 /** Represents a physical button. */
 export interface Button extends Resource {
-  /** Push the button. */
+  /**
+   * Push the button.
+   *
+   * @example
+   *
+   * ```ts
+   * const button = new VIAM.ButtonClient(machine, 'my_button');
+   *
+   * // Push the button
+   * await button.push();
+   * ```
+   */
   push: (extra?: Struct) => Promise<void>;
 }
--- src/components/switch/switch.ts
+++ src/components/switch/switch.ts
@@ -3,12 +3,58 @@ import type { Resource } from '../../types';
 
 /** Represents a physical switch with multiple positions. */
 export interface Switch extends Resource {
-  /** Set the switch to a specific position. */
+  /**
+   * Set the switch to a specific position.
+   *
+   * @example
+   *
+   * ```ts
+   * const mySwitch = new VIAM.SwitchClient(machine, 'my_switch');
+   *
+   * // Update the switch from its current position to position 1
+   * await mySwitch.setPosition(1);
+   *
+   * // Update the switch from its current position to position 0
+   * await mySwitch.setPosition(0);
+   * ```
+   */
   setPosition: (position: number, extra?: Struct) => Promise<void>;
 
-  /** Get the current position of the switch. */
+  /**
+   * Get the current position of the switch.
+   *
+   * @example
+   *
+   * ```ts
+   * const mySwitch = new VIAM.SwitchClient(machine, 'my_switch');
+   *
+   * // Update the switch to position 1
+   * await mySwitch.setPosition(1);
+   *
+   * // Get the current set position
+   * const pos1 = await mySwitch.getPosition();
+   *
+   * // Update the switch to position 0
+   * await mySwitch.setPosition(0);
+   *
+   * // Get the current set position
+   * const pos2 = await mySwitch.getPosition();
+   * ```
+   */
   getPosition: (extra?: Struct) => Promise<number>;
 
-  /** Get the total number of positions available on the switch. */
+  /**
+   * Get the total number of positions available on the switch.
+   *
+   * @example
+   *
+   * ```ts
+   * const mySwitch = new VIAM.SwitchClient(machine, 'my_switch');
+   *
+   * // Get the number of available positions
+   * const numPositions = await mySwitch.getNumberOfPositions();
+   * console.log('Number of positions:', numPositions);
+   * ```
+   */
   getNumberOfPositions: (extra?: Struct) => Promise<number>;
 }
