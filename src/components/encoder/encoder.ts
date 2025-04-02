6598190cd84abcf4ff42bd91cd2b19aa90f81dd9
JessamyT <jessamy.taylor@gmail.com>
2025-04-02 15:04:52 -0700

    DOCS-3545: Add encoder and gantry examples

--- src/components/encoder/encoder.ts
+++ src/components/encoder/encoder.ts
@@ -13,15 +13,47 @@ export const {
 
 /** Represents a physical encoder. */
 export interface Encoder extends Resource {
-  /** Set the current position of the encoder as the new zero position. */
+  /**
+   * Set the current position of the encoder as the new zero position.
+   *
+   * @example
+   *
+   * ```ts
+   * const encoder = new VIAM.EncoderClient(machine, 'my_encoder');
+   *
+   * // Reset the zero position of the encoder
+   * await encoder.resetPosition();
+   * ```
+   */
   resetPosition(extra?: Struct): Promise<void>;
 
-  /** Return the encoder's properties. */
+  /**
+   * Return the encoder's properties.
+   *
+   * @example
+   *
+   * ```ts
+   * const encoder = new VIAM.EncoderClient(machine, 'my_encoder');
+   *
+   * // Get whether the encoder returns position in ticks or degrees
+   * const properties = await encoder.getProperties();
+   * ```
+   */
   getProperties(extra?: Struct): Promise<EncoderProperties>;
 
   /**
    * Return the current position either in relative units (ticks away from a
    * zero position) or absolute units (degrees along a circle).
+   *
+   * @example
+   *
+   * ```ts
+   * const encoder = new VIAM.EncoderClient(machine, 'my_encoder');
+   *
+   * // Get the position of the encoder in ticks
+   * const [position, posType] = await encoder.getPosition(EncoderPositionType.POSITION_TYPE_TICKS_COUNT);
+   * console.log('The encoder position is currently', position, posType);
+   * ```
    *
    * @param positionType - The type of position the encoder returns (ticks or
    *   degrees)
