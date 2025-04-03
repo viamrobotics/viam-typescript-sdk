825a70c14cbcc09eea4c97c3b203a590fca34e16
Jessamy Taylor <75634662+JessamyT@users.noreply.github.com>
2025-04-03 16:02:01 -0700

    DOCS-3542: Add motor and servo code samples (#521)

--- src/components/servo/servo.ts
+++ src/components/servo/servo.ts
@@ -3,15 +3,65 @@ import type { Resource } from '../../types';
 
 /** Represents a physical servo. */
 export interface Servo extends Resource {
-  /** Move the servo by a given angle in degrees. */
+  /**
+   * Move the servo by a given angle in degrees.
+   *
+   * @example
+   *
+   * ```ts
+   * const servo = new VIAM.ServoClient(machine, 'my_servo');
+   *
+   * // Move the servo from its origin to the desired angle of 10 degrees
+   * await servo.move(10);
+   *
+   * // Move the servo from its origin to the desired angle of 90 degrees
+   * await servo.move(90);
+   * ```
+   */
   move(angleDeg: number, extra?: Struct): Promise<void>;
 
-  /** Return the current set angle of the servo in degrees. */
+  /**
+   * Return the current set angle of the servo in degrees.
+   *
+   * @example
+   *
+   * ```ts
+   * const servo = new VIAM.ServoClient(machine, 'my_servo');
+   *
+   * // Get the current set angle of the servo
+   * const pos = await servo.getPosition();
+   * ```
+   */
   getPosition(extra?: Struct): Promise<number>;
 
-  /** Stop the servo. */
+  /**
+   * Stop the servo.
+   *
+   * @example
+   *
+   * ```ts
+   * const servo = new VIAM.ServoClient(machine, 'my_servo');
+   *
+   * // Move the servo from its origin to the desired angle of 10 degrees
+   * await servo.move(10);
+   *
+   * // Stop the servo. It is assumed that the servo stops moving immediately
+   * await servo.stop();
+   * ```
+   */
   stop(extra?: Struct): Promise<void>;
 
-  /** Return true if the servo is in motion. */
+  /**
+   * Return true if the servo is in motion.
+   *
+   * @example
+   *
+   * ```ts
+   * const servo = new VIAM.ServoClient(machine, 'my_servo');
+   *
+   * const moving = await servo.isMoving();
+   * console.log('Moving:', moving);
+   * ```
+   */
   isMoving(): Promise<boolean>;
 }
