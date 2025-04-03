f995b43f599028ae75f4ebc11bd9bf1e5611cd70
Jessamy Taylor <75634662+JessamyT@users.noreply.github.com>
2025-04-03 16:02:18 -0700

    DOCS-3543: Add generic component and gripper code samples (#523)

--- src/components/generic/generic.ts
+++ src/components/generic/generic.ts
@@ -3,6 +3,21 @@ import type { Struct, Resource } from '../../types';
 
 /** Represents a generic component. */
 export interface Generic extends Resource {
-  /** Get the geometries of the component in their current configuration */
+  /**
+   * Get the geometries of the component in their current configuration.
+   *
+   * @example
+   *
+   * ```ts
+   * const generic = new VIAM.GenericComponentClient(
+   *   machine,
+   *   'my_generic_component'
+   * );
+   *
+   * // Get the geometries of this component
+   * const geometries = await generic.getGeometries();
+   * console.log('Geometries:', geometries);
+   * ```
+   */
   getGeometries: (extra?: Struct) => Promise<Geometry[]>;
 }
