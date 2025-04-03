3751d7ea76d103a5d1d037fd0aacdc052a057d03
Jessamy Taylor <75634662+JessamyT@users.noreply.github.com>
2025-04-03 16:01:26 -0700

    DOCS-3544: Add input controller examples (#525)

--- src/components/input-controller/input-controller.ts
+++ src/components/input-controller/input-controller.ts
@@ -10,13 +10,47 @@ export const { Event: InputControllerEvent } = pb;
  * events for controls.
  */
 export interface InputController extends Resource {
-  /** Returns a list of events representing the last event on each control. */
+  /**
+   * Returns a list of events representing the last event on each control.
+   *
+   * @example
+   *
+   * ```ts
+   * const controller = new VIAM.InputControllerClient(
+   *   machine,
+   *   'my_controller'
+   * );
+   *
+   * // Get the most recent Event for each Control
+   * const recentEvents = await controller.getEvents();
+   * console.log('Recent events:', recentEvents);
+   * ```
+   */
   getEvents(extra?: Struct): Promise<InputControllerEvent[]>;
 
   /**
    * TriggerEvent, where supported, injects an InputControllerEvent into an
    * input controller to (virtually) generate events like button presses or axis
-   * movements
+   * movements.
+   *
+   * @example
+   *
+   * ```ts
+   * const controller = new VIAM.InputControllerClient(
+   *   machine,
+   *   'my_controller'
+   * );
+   *
+   * // Create a "Button is Pressed" event for the control BUTTON_START
+   * const buttonPressEvent = new VIAM.InputControllerEvent({
+   *   time: { seconds: BigInt(Math.floor(Date.now() / 1000)) },
+   *   event: 'ButtonPress',
+   *   control: 'ButtonStart',
+   *   value: 1.0,
+   * });
+   * // Trigger the event
+   * await controller.triggerEvent(buttonPressEvent);
+   * ```
    */
   triggerEvent(event: InputControllerEvent, extra?: Struct): Promise<void>;
 }
