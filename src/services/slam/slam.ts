c640dffcdf4e45278788ef46bb6ff75c10d26e87
JessamyT <jessamy.taylor@gmail.com>
2025-04-04 14:21:54 -0700

    DOCS-3552: Add SLAM examples

--- src/services/slam/slam.ts
+++ src/services/slam/slam.ts
@@ -9,18 +9,63 @@ export interface Slam extends Resource {
   /**
    * Get the current position of the specified source component in the point
    * cloud SLAM map.
+   *
+   * @example
+   *
+   * ```ts
+   * const slam = new VIAM.SlamClient(machine, 'my_slam');
+   *
+   * // Get the current position of the robot in the SLAM map
+   * const position = await slam.getPosition();
+   * console.log('Current position:', position);
+   * ```
    */
   getPosition: () => Promise<SlamPosition>;
 
-  /** Get the point cloud SLAM map. */
+  /**
+   * Get the point cloud SLAM map.
+   *
+   * @example
+   *
+   * ```ts
+   * const slam = new VIAM.SlamClient(machine, 'my_slam');
+   *
+   * // Get the point cloud map
+   * const pointCloudMap = await slam.getPointCloudMap();
+   *
+   * // Get the edited point cloud map
+   * const editedMap = await slam.getPointCloudMap(true);
+   * ```
+   */
   getPointCloudMap: (returnEditedMap?: boolean) => Promise<Uint8Array>;
 
   /**
    * Get the internal state of the SLAM algorithm required to continue
    * mapping/localization.
+   *
+   * @example
+   *
+   * ```ts
+   * const slam = new VIAM.SlamClient(machine, 'my_slam');
+   *
+   * // Get the internal state of the SLAM algorithm
+   * const internalState = await slam.getInternalState();
+   * ```
    */
   getInternalState: () => Promise<Uint8Array>;
 
-  /** Gets information on the properties of the current SLAM service. */
+  /**
+   * Gets information on the properties of the current SLAM service.
+   *
+   * @example
+   *
+   * ```ts
+   * const slam = new VIAM.SlamClient(machine, 'my_slam');
+   *
+   * // Get the properties of the SLAM service
+   * const properties = await slam.getProperties();
+   * console.log('SLAM properties:', properties);
+   * ```
+   */
   getProperties: () => Promise<SlamProperties>;
 }
