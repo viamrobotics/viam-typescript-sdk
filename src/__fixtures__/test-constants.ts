9502f3b65d4e5005590fe76939e9b3e0f4ea3e27
Devin C <devin@viam.com>
2025-11-10 14:00:51 -0500

    fix eslint ignores and format

--- src/__fixtures__/test-constants.ts
+++ src/__fixtures__/test-constants.ts
@@ -2,4 +2,3 @@ export const TEST_URL = 'http://base.test';
 export const TEST_HOST = 'test-host';
 export const TEST_AUTH_ENTITY = 'test-entity';
 export const TEST_SIGNALING_ADDRESS = 'https://signaling.test';
-
