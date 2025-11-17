59f18fcb121f9e74485a2369bbd950559286f166
Devin C <devin@viam.com>
2025-11-17 13:09:52 -0500

    fix setup.sh

--- e2e/setup.sh
+++ e2e/setup.sh
@@ -9,7 +9,6 @@ BIN_PATH=$BIN_DIR/$BIN_NAME
 
 mkdir -p $BIN_DIR
 
-# Download the same stable version across all platforms
 if [ "$(uname)" == "Linux" ]; then
 	if [ "$(uname -m)" == "aarch64" ]; then
 		curl -fsSL https://storage.googleapis.com/packages.viam.com/apps/viam-server/viam-server-stable-aarch64 -o $BIN_PATH
@@ -21,13 +20,14 @@ if [ "$(uname)" == "Linux" ]; then
 	fi
 	chmod +x $BIN_PATH
 elif [ "$(uname)" == "Darwin" ]; then
-	if [ "$(uname -m)" == "arm64" ]; then
-		curl -fsSL https://storage.googleapis.com/packages.viam.com/apps/viam-server/viam-server-stable-aarch64 -o $BIN_PATH
-	elif [ "$(uname -m)" == "x86_64" ]; then
-		curl -fsSL https://storage.googleapis.com/packages.viam.com/apps/viam-server/viam-server-stable-x86_64 -o $BIN_PATH
+	brew tap viamrobotics/brews 2>/dev/null || true
+	
+	if brew list viam-server &>/dev/null; then
+		brew upgrade viam-server || echo "viam-server is already up to date"
 	else
-		echo -e "Cannot run E2E tests on $(uname -m)"
-		exit 1
+		brew install viam-server
 	fi
+
+	ln -s $(which viam-server) $BIN_PATH
 	chmod +x $BIN_PATH
 fi
